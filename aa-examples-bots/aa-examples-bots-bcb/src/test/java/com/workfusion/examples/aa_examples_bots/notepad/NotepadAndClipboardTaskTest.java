a05435d3e16daa6bd6f46b32013d31297a6319f3
abrakovich <96831564+abrakovich@users.noreply.github.com>
2022-04-12 16:54:09 +0200

    Add files via upload

--- aa-examples-bots/aa-examples-bots-bcb/src/test/java/com/workfusion/examples/aa_examples_bots/notepad/NotepadAndClipboardTaskTest.java
+++ aa-examples-bots/aa-examples-bots-bcb/src/test/java/com/workfusion/examples/aa_examples_bots/notepad/NotepadAndClipboardTaskTest.java
@@ -3,11 +3,9 @@ package com.workfusion.examples.aa_examples_bots.notepad;
 import com.workfusion.examples.aa_examples_bots.AbstractTestWithLogger;
 import com.workfusion.examples.aa_examples_bots.notepad.task.NotepadAndClipboardTask;
 import com.workfusion.odf.test.junit.IacDeveloperJUnitConfig;
-import com.workfusion.odf.test.launch.BotTaskResult;
 import com.workfusion.odf.test.launch.InputData;
-import com.workfusion.odf.test.launch.LaunchSettingsFactory;
-import com.workfusion.odf.test.launch.WorkerAgent;
-import com.workfusion.studio.bot.model.LaunchSettings;
+import com.workfusion.odf.test.launch.OutputData;
+import com.workfusion.odf2.junit.BotTaskFactory;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,18 +13,15 @@ import org.junit.jupiter.api.Test;
 public class NotepadAndClipboardTaskTest extends AbstractTestWithLogger {
 
     private static final String TEXT_TO_TYPE = "Test text";
+
     @Test
     @DisplayName("Should run bot task")
-    void shouldRunRpaBotTask(WorkerAgent agent, LaunchSettingsFactory settingsFactory) {
+    void shouldRunRpaBotTask(BotTaskFactory botTaskFactory) {
 
+        OutputData outputData = botTaskFactory.fromClass(NotepadAndClipboardTask.class)
+                .withInputData(InputData.of(NotepadAndClipboardTask.INPUT_COLUMN_NAME, TEXT_TO_TYPE))
+                .buildAndRun();
 
-        LaunchSettings settings = settingsFactory.builder()
-                .botTask("configs/main/notepad-and-clipboard.xml")
-                .inputData(InputData.of(NotepadAndClipboardTask.INPUT_COLUMN_NAME, TEXT_TO_TYPE))
-                .build();
-
-        BotTaskResult actualResult = agent.run(settings);
-
-        logResults(actualResult.getOutputData());
+        logResults(outputData);
     }
 }
